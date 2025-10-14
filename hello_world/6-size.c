67ec6b64ac6c02c5c3c703908c32cddc74cfe3ca
TheKasman <kasapidisandrew@gmail.com>
2025-10-14 02:32:41 +0000

    COMMAS

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int l;
 	float m;
 
-	printf("Size of a char: " sizeof(i));
-	printf("Size of an int: " sizeof(j));
-	printf("Size of an long int: " sizeof(k));
-	printf("Size of an long long int: " sizeof(l));
-	printf("Size of an float: " sizeof(m));
+	printf("Size of a char: ", sizeof(i));
+	printf("Size of an int: ", sizeof(j));
+	printf("Size of an long int: ", sizeof(k));
+	printf("Size of an long long int: ", sizeof(l));
+	printf("Size of an float: ", sizeof(m));
 
 	return (0);
 }
