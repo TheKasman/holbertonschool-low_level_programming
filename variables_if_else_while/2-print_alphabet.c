834083bd523fc31863ccf904687975f6d3550f14
TheKasman <kasapidisandrew@gmail.com>
2025-10-16 02:35:54 +0000

    yay

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Main loop to do the task
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < alphabet['\0']; i++)
+	{
+		putchar(alphabet[i]);
+		putchar('\n');
+	}
+	return (0);
+}
