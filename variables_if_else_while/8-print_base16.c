b9f116e70126d7ea310a5ca8339d674c3463beec
TheKasman <kasapidisandrew@gmail.com>
2025-10-17 13:28:22 +1100

    to the bit

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - main loop!
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int hex;
+
+	for (hex = 0; hex > 16; hex++)
+	{
+		if (hex < 10)
+		{
+			putchar('0' + hex);
+		}
+		else
+		{
+			putchar('a' + (hex - 10));
+		}
+	}
+	putchar('\n');
+	return (0);
+}
