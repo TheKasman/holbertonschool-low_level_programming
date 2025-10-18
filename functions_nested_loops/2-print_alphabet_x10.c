814e34d9c65a9c823c638274095de6271ad024d6
TheKasman <kasapidisandrew@gmail.com>
2025-10-19 10:51:13 +1100

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+void _print_alphabet_x10(void);
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 26; i++)
+		{
+			_putchar('a' + i);
+		}
+		_putchar('\n');
+	}
+}
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
