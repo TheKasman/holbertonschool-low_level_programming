cb69424eb035e83ddaf31b50d18979426618a69d
TheKasman <kasapidisandrew@gmail.com>
2025-10-19 10:25:45 +1100

    eys

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,21 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-void print_alphabet(void);
-
-/**
- * main - main loop
- *
- * Return: always 0 (success)
- */
-
-int main(void)
-{
-	print_alphabet(); /*whee*/
-	return (0);
-}
-
-
 /**
  * print_alphabet - prints the alphabet in lowercase
  *
@@ -31,4 +16,16 @@ void print_alphabet(void)
 		_putchar('a' + i);
 	}
 	_putchar('\n');
+}
+
+/**
+ * main - Main loop
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
