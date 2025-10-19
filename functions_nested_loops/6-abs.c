edf6ee6522dcb25c18b268f574a64a5b9ad39508
TheKasman <kasapidisandrew@gmail.com>
2025-10-20 10:46:59 +1100

    fixed

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -6,42 +6,26 @@ int _abs(int a);
 
 /**
  * _abs - shows the absolute value of the input
- * @a: the integer in question
+ * @a- the integer in question
  *
- * Return: returns absolute value
+ * Return: returns absolute value (a if positive, num if negative)
  */
 
 int _abs(int a)
 {
 	int num;
-	if ( a > '0')
+	if ( a > 0)
 	{
-		num = a * 1;
-		return (num);
+		return (a);
 	}
-	else if ( a < '0')
+	else if ( a < 0)
 	{
 		num = a * -1;
 		return (num);
 	}
 	else
 	{
-		num = '0';
+		num = 0;
 		return (num);
 	}
-}
-
-int main(void)
-{
-    int r;
-
-    r = _abs(-1);
-    printf("%d\n", r);
-    r = _abs(0);
-    printf("%d\n", r);
-    r = _abs(1);
-    printf("%d\n", r);
-    r = _abs(-98);
-    printf("%d\n", r);
-    return (0);
 }
