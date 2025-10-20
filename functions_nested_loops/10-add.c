d21a305eaebfdd7df622ac384fbb1519aee9d1be
TheKasman <kasapidisandrew@gmail.com>
2025-10-20 16:27:15 +1100

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+int add(int, int);
+
+/**
+ * add - adds two intervals together
+ * @int, @int - add int 1 to int 2
+ * Return: the result of adding them together (result)
+ */
+
+int add(int a, int b)
+{
+	int sum;
+	sum = a + b;
+	return (sum);
+}
