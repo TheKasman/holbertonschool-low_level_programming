5c3532cca75932212b87772cce9ade15506e2b41
TheKasman <kasapidisandrew@gmail.com>
2025-10-21 10:06:18 +1100

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+int mul(int, int);
+
+/**
+ * mul - multiplies two intervals together
+ * @a: first value
+ * @b: second value
+ * Return: the result of multiplying them together (result)
+ */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+	return (sum);
+}
