8c860d3f63fa1f6c3318a9f90602ebf3dc2a8ae8
TheKasman <kasapidisandrew@gmail.com>
2025-10-20 16:16:55 +1100

    thanks betty

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,33 +10,33 @@ void times_table(void);
 
 void times_table(void)
 {
-    int i, j, value;
+	int i, j, value;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            value = i * j;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			value = i * j;
 
-            if (j == 0)
-            {
-                _putchar('0' + value);
-            }
-            else if (value >= 10)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar('0' + value / 10);
-                _putchar('0' + value % 10);
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
-                _putchar('0' + value);
-            }
-        }
-        _putchar('\n');
-    }
+			if (j == 0)
+			{
+				_putchar('0' + value);
+			}
+			else if (value >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + value / 10);
+				_putchar('0' + value % 10);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + value);
+			}
+		}
+		_putchar('\n');
+	}
 }
