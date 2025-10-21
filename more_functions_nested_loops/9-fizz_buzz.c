68df9666f250b60b4e84e13a9dde96d9cc722452
TheKasman <kasapidisandrew@gmail.com>
2025-10-21 12:58:11 +1100

    yes

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+void fizzBuzz(void);
+
+/**
+ * fizzBuzz - IT'S FIZZBUZZ TIME BABYYYY
+ * Return: void
+ */
+
+void fizzBuzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+
+
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+
+		/*print out numbers (FOR LOOP)*/
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+}
