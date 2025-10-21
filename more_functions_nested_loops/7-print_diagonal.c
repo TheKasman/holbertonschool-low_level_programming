950a53c1ad5824ce065ef7cb84ddb7eddafd63cb
TheKasman <kasapidisandrew@gmail.com>
2025-10-21 11:54:34 +1100

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_diagonal(int);
+
+/**
+ * print_diagonal - prints a diagonal line based off the value of n "\"
+ * @n : the value based off an int
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
