81b7e9a48db80c82a8291c5c88d8be462f15aa64
TheKasman <kasapidisandrew@gmail.com>
2025-10-23 15:30:46 +1100

    maybe?

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -14,11 +14,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while ((str[i] != '\0'))
+	while ((i == 0) || ((str[i - 1] != '\0' && str[i] != '\0')))
 	{
 		_putchar(str[i]);
-		i = i + 2;
-
+		i += 2;
 	}
 	_putchar('\n');
 }
