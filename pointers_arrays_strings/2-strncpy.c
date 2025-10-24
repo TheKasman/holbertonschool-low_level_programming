4649ae17765c2067d08675aa37a6e81e24893321
TheKasman <kasapidisandrew@gmail.com>
2025-10-25 10:45:53 +1100

    2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strncpy - Copies a string to another
+ * @dest: destination
+ * @src: the stuff to copy from.
+ * @n: the amount of data to be copied to the destination
+ * Return: Returns the fused product as a char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *fused = dest;
+	int i = 0;
+
+	while (i < n && *src != '\0')
+	{
+		*fused = *src;
+		fused++;
+		src++;
+		i++;
+	}
+
+	*fused = '\0';
+
+	return (dest);
+}
