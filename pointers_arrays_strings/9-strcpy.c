336592909f35a6d04cd3268f79713db910488d26
TheKasman <kasapidisandrew@gmail.com>
2025-10-24 11:00:40 +1100

    9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - copies the string to a new destination
+ * @dest: the destination
+ * @src: the source
+ * Return: Returns the destination pointer
+ */
+
+
+char *_strcpy(char *dest, char *src)
+{
+	char *point = dest;
+
+	/*loop the source string until the null terminator*/
+	while (*src != '\0')
+	{
+		*point = *src;
+
+		point++;
+		src++;
+	}
+	return (dest);
+}
