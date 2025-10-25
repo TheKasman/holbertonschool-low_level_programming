a452a96aa23a447a66cdcc67e0cd9aa9b2e271d0
TheKasman <kasapidisandrew@gmail.com>
2025-10-25 14:06:54 +1100

    1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *leet - Changes specific letters to become 1337 sp34k
+ *@c: the string
+ * Return: returns the 1337 string
+ */
+
+char *leet(char *c)
+{
+	char letter[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char leet[] = {'4', '4', '3', '3', '0', '0', '1', '1', '5', '5'};
+	int i, j;
+
+	for (i = 0; c[i]; i++)
+	{
+		for (j = 0; j < 12; j++)
+		{
+			if (c[i] == letter[j])
+			{
+				c[i] = leet[j];
+			}
+		}
+	}
+	return (c);
+}
