037ace4593a4b9099131fb18e9a2afe1e0748398
TheKasman <kasapidisandrew@gmail.com>
2025-10-29 13:39:04 +1100

    maybe?

---
+++ 0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc:our argument counter
+ * @argv:argument vector
+ * Return: 0 (always successful)
+ */
+
+int main(int argc, int argv[])
+{
+	printf("%d\n", argv[0]);
+	return (0);
+}
