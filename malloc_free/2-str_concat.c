056a1829a259e1647623edd93e96cf54268c4044
TheKasman <kasapidisandrew@gmail.com>
2025-11-03 13:31:57 +1100

    maybe?

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,6 +16,11 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	/*move to the end of string 1*/
 	for (i = 0; s1[i]; i++)
 	{}
