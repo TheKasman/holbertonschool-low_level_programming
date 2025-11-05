f84347d8fb3ac6df141b2c2c544887d072df4bc7
TheKasman <kasapidisandrew@gmail.com>
2025-11-05 15:16:57 +1100

    yes here we go

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - the doggo struct
  * @name: doggo name
@@ -15,5 +18,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
