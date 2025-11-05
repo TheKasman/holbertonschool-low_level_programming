1c80e16336c39424bbb27d13901fe7e380b607e0
TheKasman <kasapidisandrew@gmail.com>
2025-11-05 15:07:57 +1100

    yes

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "dog.h"
+
+/**
+ * init_dog - initalises the dog object
+ * @d: the structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the doggo
+ * Return: it's void, no return!
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
