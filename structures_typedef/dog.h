7c2af180f71406bc7d5ab9de8630a61fc2ea3cb5
TheKasman <kasapidisandrew@gmail.com>
2025-11-05 14:45:42 +1100

    init

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - the doggo struct
+ * @name: doggo name
+ * @age: doggo age
+ * @owner: doggo's handler
+ * Description: A struct to make a doggo!
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
