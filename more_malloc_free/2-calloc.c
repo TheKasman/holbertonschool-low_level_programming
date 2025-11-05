b56ed9c934d7005ad85f3bc9635f268353f8a03c
TheKasman <kasapidisandrew@gmail.com>
2025-11-05 12:41:55 +1100

    okay...

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * *_calloc - CALLOC THESE
+ * @nmemb:amonut of elements
+ * @size: amount of bytes each element is
+ * Return: returns the void pointer back from the void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	/*If malloc fails, then _calloc returns NULL*/
+	unsigned int print_nmemb;
+	void *memory;
+	char *zerome;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	/*print garbage*/
+	memory = malloc(nmemb * size);
+
+	/*cast void* as char* */
+	zerome = memory;
+
+	/*print zeroed values in garbage*/
+	for (print_nmemb = 0; print_nmemb < nmemb * size; print_nmemb++)
+	{
+		zerome[print_nmemb] = 0;
+	}
+	return (memory);
+}
