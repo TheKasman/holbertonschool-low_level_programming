3279415a326db5fece7461054b3fbdec2b92a4d4
TheKasman <kasapidisandrew@gmail.com>
2025-11-10 13:54:22 +1100

    One more to go!

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: the array we're searching
+ * @size: number of elements
+ * @cmp:functions to use
+ * Return: returns the index, -1 if no match or an error
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int search_index;
+
+	/*check for errors on all variables first*/
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
+	/*time to search!*/
+	for (search_index = 0; search_index < size; search_index++)
+	{
+		cmp(array[search_index]);
+	}
+
+	return (search_index);
+}
