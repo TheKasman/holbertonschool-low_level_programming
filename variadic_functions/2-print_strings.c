2b7d363affa0fa07616540a504fd81751626d940
TheKasman <kasapidisandrew@gmail.com>
2025-11-13 12:21:21 +1100

    whee

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings!
+ * @separator: the separaTOORRRRR
+ * @n: the amount of things to count
+ * @...: our arguments
+ */
+
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	char *arg;
+
+	va_start(args, n);
+
+
+	for (i = 0; i < n; i++)
+	{
+		arg = va_arg(args, char *);
+
+		if (arg != NULL)
+			printf("%s", arg);
+		else
+			printf("(nil)");
+
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
--- variadic_functions/header.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEADER_H
-#define HEADER_H
-
-#include <stdio.h>
-#include <stdarg.h>
-
-int sum_them_all(const unsigned int n, ...);
-
-#endif
