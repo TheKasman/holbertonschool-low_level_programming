550b4d3d34acab3aa2d36f5bc1abd3a1e33cc269
TheKasman <kasapidisandrew@gmail.com>
2025-11-13 14:57:50 +1100

    wheeeeeeeeeeeeeeeee

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -38,8 +38,7 @@ void print_all(const char * const format, ...)
 				break;
 			case 's':
 				str = va_arg(args, char *);
-				if (str)
-					printf("%s", str);
+				printf("%s", str);
 				if (!str)
 					printf("(nil)");
 				printed = 1;
