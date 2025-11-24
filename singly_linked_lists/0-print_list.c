365f75fcf42a5bdde65e24bde4491ff192eb9c2c
TheKasman <kasapidisandrew@gmail.com>
2025-11-25 10:13:30 +1100

    fixed

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,15 +10,18 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-	}
 	while (h->str != NULL)
 	{
-		printf("[%u] %s\n", h->len, h->str);
-		count++;
-		h = h->next;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			count++;
+			h = h->next;
+		}
 	}
 	return (count);
 }
