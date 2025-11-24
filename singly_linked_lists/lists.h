f997ecf5f7428441752cac67484b2e0b35cfc8dc
TheKasman <kasapidisandrew@gmail.com>
2025-11-25 10:03:51 +1100

    fixed my includes

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -24,5 +24,7 @@ typedef struct list_s
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+
 
 #endif
