76178871396f4f8c336bd49ffad32544b9b38312
TheKasman <kasapidisandrew@gmail.com>
2025-11-25 13:44:28 +1100

    question 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints the doubly linked list
+ * @h: the linked list
+ * Return: returns the count of the linked list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
