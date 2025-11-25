aac9c7840af0172699f3e13c6c1f62954b35e274
TheKasman <kasapidisandrew@gmail.com>
2025-11-25 11:04:14 +1100

    question 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node to the linked list
+ * @head: the head of the linked list
+ * @str: the string?
+ * Return: returns the list!
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	size_t nchar = 0;
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->len = nchar;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
