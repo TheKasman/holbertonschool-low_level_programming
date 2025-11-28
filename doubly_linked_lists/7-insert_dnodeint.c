e0248502614d5f6139d3e4437161d86823cdaf0b
TheKasman <kasapidisandrew@gmail.com>
2025-11-28 11:51:26 +1100

    iteration 1 for question 7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+
+
+/**
+ * insert_dnodeint_at_index - inserts a node into a linked list at an index
+ * @h: the list
+ * @idx: the index where the node goes
+ * @n: the value
+ * Return: Returns the node/list back
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	int i;
+
+	/*node creation*/
+	dlistint_t *node = add_dnodeint(h, n);
+
+	/*our head check*/
+	if (idx == 0)
+	{
+		node->next = *h;
+
+		if (*h != NULL)
+			(*h)->prev = node;
+
+		*h = node;
+		return (node);
+	}
+
+	/*our temp value*/
+	dlistint_t *current = *h;
+
+	/*find the previous node before the insertion point*/
+	for (i = 0; i < idx - 1 && current != NULL; ++i)
+		current = current->next;
+
+	/*idx is out of bounds*/
+	if (current == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+
+	/*node stitching*/
+	node->prev = current;
+	node->next = current->next;
+	current->next = node;
+
+	if (node->next != NULL)
+		node->next->prev = node;
+
+	return (node);
+}
