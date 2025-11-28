550d9b35eb5154658d341bd0cedd19c80f54f46f
TheKasman <kasapidisandrew@gmail.com>
2025-11-28 12:05:48 +1100

    fixed malloc and betty

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,13 +13,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i;
 
-	/*node creation*/
 	dlistint_t *node, *current;
 
 	node = malloc(sizeof(dlistint_t));
+	if (!node)
+		return (NULL);
+
 	node->n = n;
 
-	/*our head check*/
 	if (idx == 0)
 	{
 		node->next = *h;
@@ -31,21 +32,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (node);
 	}
 
-	/*our temp value*/
 	current = *h;
 
-	/*find the previous node before the insertion point*/
 	for (i = 0; i < idx - 1 && current != NULL; ++i)
 		current = current->next;
 
-	/*idx is out of bounds*/
 	if (current == NULL)
 	{
 		free(node);
 		return (NULL);
 	}
 
-	/*node stitching*/
 	node->prev = current;
 	node->next = current->next;
 	current->next = node;
