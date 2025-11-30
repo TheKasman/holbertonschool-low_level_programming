a06cfcde719ef9e4126d9474cab87b2ed4e9b380
TheKasman <kasapidisandrew@gmail.com>
2025-11-30 15:25:19 +1100

    last test case

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,3 +1,8 @@
+#ifndef ERR_FD
+#define ERR_FD STDERR_FILENO
+#endif
+
+
 #include "main.h"
 
 /**
@@ -11,39 +16,38 @@ int main(int argc, char *argv[])
 {
 	char buffer[1024];
 	ssize_t bytesRead, bytesWritten;
-	int fd_from;
-	int fd_to;
+	int fd_from, fd_to;
 
 	if (argc != 3)
-	{	dprintf(2, "Usage: cp file_from file_to\n");
+	{	dprintf(ERR_FD, "Usage: cp file_from file_to\n");
 		exit(97); }
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-	{	dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+	{	dprintf(ERR_FD, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{	dprintf(2, "Error: Can't write to %s\n", argv[2]);
+	{	dprintf(ERR_FD, "Error: Can't write to %s\n", argv[2]);
 		exit(99); }
 	while ((bytesRead = read(fd_from, buffer, 1024)) > 0)
 	{
 		bytesWritten = write(fd_to, buffer, bytesRead);
 		if (bytesWritten == -1)
-		{	dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		{	dprintf(ERR_FD, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
 			exit(99); }
 	}
 	if (bytesRead == -1)
-	{	dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+	{	dprintf(ERR_FD, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
 		exit(98); }
 	if (close(fd_from) == -1)
-	{	dprintf(2, "Error: Can't close fd %d\n", fd_from);
+	{	dprintf(ERR_FD, "Error: Can't close fd %d\n", fd_from);
 		exit(100); }
 	if (close(fd_to) == -1)
-	{	dprintf(2, "Error: Can't close fd %d\n", fd_to);
+	{	dprintf(ERR_FD, "Error: Can't close fd %d\n", fd_to);
 		exit(100); }
 	return (0);
 }
