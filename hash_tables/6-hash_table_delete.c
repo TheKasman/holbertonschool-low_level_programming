9a321b64f0d0a85c61f0d525f1598794009fb2ba
TheKasman <kasapidisandrew@gmail.com>
2025-12-04 15:26:52 +1100

    all done?

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes the hash table!
+ * @ht: the hash table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current, *temp;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		while (current)
+		{
+			temp = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+
+			current = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
