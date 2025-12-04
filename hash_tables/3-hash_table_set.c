d373c074202ed9a3a0658875ba53d5d8f9b8ebc8
TheKasman <kasapidisandrew@gmail.com>
2025-12-04 14:41:00 +1100

    fixed that last part

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,20 +10,31 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node;
+	hash_node_t *node, *current;
 	unsigned long int keyPos;
-
-	node = malloc(sizeof(hash_node_t));
-
-	if (node == NULL)
-		return (0);
 
 	/*are the elements bad?*/
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
-	/*get our key position to slap into the hash table*/
 	keyPos = key_index((unsigned char *)key, ht->size);
+	current = ht->array[keyPos];
+
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
+
+	node = malloc(sizeof(hash_node_t));
+
+	if (node == NULL)
+		return (0);
 
 	node->key = strdup(key);
 	node->value = strdup(value);
